e7187cccaabcf1f75366f014bb91b925c81975ac
Henitz <33755649+Henitz@users.noreply.github.com>
2024-01-11 17:11:03 -0300

    projeto final

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import zipfile
+import os
+
+st.title("Aplicativo Streamlit para Processar Arquivo .zip")
+
+# Carregar o arquivo .zip
+zip_file_path = os.path.join("..", "data", "app.zip")
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    # Extraindo o conteúdo para um diretório temporário
+    temp_dir = "temp_extracted"
+    zip_ref.extractall(temp_dir)
+
+    # Listando os arquivos extraídos
+    extracted_files = os.listdir(temp_dir)
+
+# Exibindo a lista de arquivos extraídos
+st.write("Arquivos extraídos:")
+for file in extracted_files:
+    st.write(file)
