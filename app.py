80e3fc82a1b4c25a5e075b0030f213d38760812b
Henitz <33755649+Henitz@users.noreply.github.com>
2024-01-12 07:30:53 -0300

    novamente app.py

--- app.py
+++ app.py
@@ -3,37 +3,44 @@ import os
 import zipfile
 import shutil
 
+# Limpeza do diretório temporário antes de extrair o ZIP
+shutil.rmtree("temp_extracted", ignore_errors=True)
+
+# Função para extrair arquivos ZIP
+def extract_zip(zip_path, temp_dir):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(temp_dir)
+
+# Função para limpar o diretório temporário
+def clean_temp_dir(temp_dir):
+    for root, dirs, files in os.walk(temp_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            os.remove(file_path)
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            os.rmdir(dir_path)
+
+# Configuração do Streamlit
+st.title("Aplicativo Streamlit para Processar Arquivo .zip")
+
 # Diretório temporário para extração
 temp_dir = "temp_extracted"
 
 # Verifica se o diretório temporário existe, senão, cria
 if not os.path.exists(temp_dir):
     os.makedirs(temp_dir)
-else:
-    st.warning("O diretório temporário já existe. Tentando removê-lo.")
 
-    # Tentar remover o diretório temporário
-    try:
-        shutil.rmtree(temp_dir)
-        os.makedirs(temp_dir)
-    except Exception as e:
-        st.error(f"Erro ao tentar remover o diretório temporário: {e}")
+# Mensagem para o usuário fornecer o caminho do arquivo ZIP
+zip_path = st.text_input("Digite o caminho do arquivo ZIP:")
 
-# Obtém o caminho absoluto do diretório do script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Constrói o caminho para o arquivo ZIP
-zip_path = os.path.join(script_dir, 'data', 'app.zip')
-
-# Verifica se o arquivo ZIP existe
-if os.path.exists(zip_path):
+if st.button("Executar"):
     try:
         # Limpa o diretório temporário antes da extração
-        shutil.rmtree(temp_dir, ignore_errors=True)
+        clean_temp_dir(temp_dir)
 
         # Extrai os arquivos do ZIP para o diretório temporário
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(temp_dir)
+        extract_zip(zip_path, temp_dir)
 
         st.success("Arquivo ZIP extraído com sucesso!")
 
@@ -44,6 +51,3 @@ if os.path.exists(zip_path):
 
     except Exception as e:
         st.error(f"Erro ao extrair o arquivo ZIP: {e}")
-
-else:
-    st.warning("O arquivo ZIP não foi encontrado. Verifique o caminho.")
